d04199b1a93132f00d211838ed979a995fa78256
luna shin <mshi2784@uni.sydney.edu.au>
2025-04-27 02:10:07 +1000

    updating narwhal page

--- src/app/project-4/page.tsx
+++ src/app/project-4/page.tsx
@@ -8,9 +8,9 @@ export default function Project() {
     <main className={styles.main}>
       <Heading title={"Narwhal Tutoring"} description={"Active & Commercialised tutoring website developed on Django"}/>
       <Link className="button" href="/">Go back</Link>
-      <p>I have worked on this online tutoring website from scratch as a team outside of university and commercialised it.
+      <p style={{ maxWidth: "600px" }}>I have worked on this online tutoring website from scratch as a team outside of university and commercialised it.
         It uses Django framework. I focused on the features such as review systems and front-end designs.</p>
-      <Image width={140*2} height={100*2} src="/narwhal.png" alt="Project" />
+      <Image width={180*2} height={100*2} src="/narwhal.png" alt="Project" />
       <p>Click the button below to see the project in web!</p>
       <Link className="button" href="https://www.narwhaltutoring.com/">Visit Project</Link>
     </main>
